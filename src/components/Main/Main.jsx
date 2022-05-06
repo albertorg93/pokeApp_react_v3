f67929014436fe7baa33a6a10a46fb9fe4a3a0c8
albertorg93 <futberto1993@gmail.com>
2022-05-06 13:24:34 +0200

    added debounce

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,11 +4,14 @@ import { Route,Routes } from 'react-router-dom'
 import Card from './Card'
 import ListaPokemon from './ListaPokemon'
 import axios from 'axios'
+import { useDebounce } from "use-debounce";
 
 export default function Main() {
   const [value, setValue] = useState(""); // Para guardar el dato a buscar
   const [pokemon, setPokemons] = useState([]); // Para guardar los posts
   const [unico, setUnico] = useState({});
+  const [input, setInput] = useState("");
+  const [debouncedInput] = useDebounce(input, 1500);
 
   // equivale a un componentDidUpdate()
   useEffect(() => {
@@ -16,7 +19,7 @@ export default function Main() {
       try{
         // Petición HTTP
         
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`);
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${debouncedInput}`);
         const json = res.data;
         setUnico(json)
         setPokemons([json,...pokemon]);
@@ -26,22 +29,29 @@ export default function Main() {
       }
     }
     fetchData();
-  }, [value]);
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log(e.target.topic.value)
-    setValue(e.target.topic.value) // Modificando el estado de Value
-    e.target.topic.value = ""
+  }, [debouncedInput]);
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   console.log(e.target.topic.value)
+  //   setValue(e.target.topic.value) // Modificando el estado de Value
+  //   e.target.topic.value = ""
 
-  };
-  
+  // };
+
+  const handleChange = e => {
+    e.preventDefault();
+    setInput(e.target.value);
+    console.log(input);
+  }
+
+
   console.log(unico)
   return (
     <main>
     <img className='logoinicial' src={logopoke} alt="logopokemon"/>
     <h1> Bienvenido a la Pokedex</h1>
-              <form onSubmit={handleSubmit}>
-                <input name="topic" className='busqueda'/>
+              <form>
+                <input name="topic" className='busqueda' onChange={handleChange} />
               </form>
     <Routes>
        <Route element={<Card data={unico}/>} path='/card'/>
